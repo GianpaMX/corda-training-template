5fbc1ecb1513b438b1899866093dd5cdcd08cf66
Juan Pablo "Gianpa" Villa <juan.villa@wizeline.com>
2018-12-12 09:34:20 -0600

    IOU Transfer Tests

--- kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
@@ -25,6 +25,7 @@ class IOUContract : Contract {
         // class DoSomething : TypeOnlyCommandData(), Commands
 
         class Issue : TypeOnlyCommandData(), Commands
+        class Transfer : TypeOnlyCommandData(), Commands
     }
 
     /**
@@ -32,17 +33,31 @@ class IOUContract : Contract {
      * The constraints are self documenting so don't require any additional explanation.
      */
     override fun verify(tx: LedgerTransaction) {
-        tx.commands.requireSingleCommand<Commands.Issue>()
-        requireThat {
-            "No inputs should be consumed when issuing an IOU." using tx.inputs.isEmpty()
-            "Only one output state should be created when issuing an IOU." using (tx.outputs.size == 1)
+        val command = tx.commands.requireSingleCommand<Commands>()
+        when (command.value) {
+            is Commands.Issue -> requireThat {
+                "No inputs should be consumed when issuing an IOU." using tx.inputs.isEmpty()
+                "Only one output state should be created when issuing an IOU." using (tx.outputs.size == 1)
 
-            val output = tx.outputs.single().data as IOUState
-            "A newly issued IOU must have a positive amount." using (output.amount.quantity > 0)
-            "The lender and borrower cannot have the same identity." using (output.lender != output.borrower)
+                val output = tx.outputs.single().data as IOUState
+                "A newly issued IOU must have a positive amount." using (output.amount.quantity > 0)
+                "The lender and borrower cannot have the same identity." using (output.lender != output.borrower)
 
-            "Both lender and borrower together only may sign IOU issue transaction." using
-                    (tx.commands.single().signers.toSet() == output.participants.map { it.owningKey }.toSet())
+                "Both lender and borrower together only may sign IOU issue transaction." using
+                        (tx.commands.single().signers.toSet() == output.participants.map { it.owningKey }.toSet())
+            }
+            is Commands.Transfer -> requireThat {
+                "An IOU transfer transaction should only consume one input state." using (tx.inputs.size == 1)
+                "An IOU transfer transaction should only create one output state." using (tx.outputs.size == 1)
+
+                val input = tx.inputs.single().state.data as IOUState
+                val output = tx.outputs.single().data as IOUState
+                "Only the lender property may change." using (input.copy(lender = output.lender) == output)
+                "The lender property must change in a transfer." using (input.lender != output.lender)
+                "The borrower, old lender and new lender only must sign an IOU transfer transaction" using
+                        (tx.commands.single().signers.toSet() ==
+                                input.participants.map { it.owningKey }.union(output.participants.map { it.owningKey }).toSet())
+            }
         }
     }
 }
